d7b1544a64bcd3c344cd3e96e0ca9cc798eea7db
Vignesh Jayabalan <vignesh.jayabalan@hashicorp.com>
2024-09-11 07:16:20 +0530

    Add a local backend config

---
+++ LocalApplication/main.tf
@@ -0,0 +1,18 @@
+terraform {
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+
+}
+
+resource "null_resource" "resource_1" {
+  provisioner "local-exec" {
+    command = "echo 'Hello, World!'"
+  }
+}
+
+output "output_1" {
+  value = "$(timestamp())"
+}
+
